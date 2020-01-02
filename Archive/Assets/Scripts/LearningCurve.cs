af3c9257e79ce4d6b02cf8c5cf655c0f2542845a
krishan vadher <krishan.vadher@gmail.com>
2020-01-02 22:24:53 +0000

    new repo

---
+++ Archive/Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    void Start()
+    {
+        string characterAction = "Heal";
+
+        switch(characterAction)
+        {
+            case "Heal":
+                Debug.Log("potion sent");
+                break;
+            case "Attack":
+                Debug.Log("to arms");
+                break;
+            default:
+                Debug.Log("shields up");
+                break;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    private Transform camTransform;
+
+    public GameObject directionLight;
+    private Transform lightTransform;
+
+    void Start()
+    {
+        int playerLives = 10;
+
+        while (playerLives > 0)
+        {
+            Debug.Log("Still alive!!!!!");
+            playerLives--;
+        }
+
+        Debug.Log("Player koooo'dd");
+
+        Character hero = new Character();
+        /*Debug.LogFormat("Hero: {0} - {1} EXP", hero.name, hero.exp);*/
+        hero.PrintStatsInfo();
+        
+        Character heroine = new Character("Agatha");
+        /*Debug.LogFormat("Hero: {0} - {1} EXP", heroine.name, heroine.exp);*/
+        heroine.PrintStatsInfo();
+
+        Weapon huntingBow = new Weapon("Hunting Bow", 105);
+        Weapon warBow = new Weapon("crazy Bow", 2);
+        huntingBow.PrintWeaponStats();
+        warBow.PrintWeaponStats();
+
+        Character hero2 = hero;
+        hero2.name = "the cool cat";
+        hero.PrintStatsInfo();
+        hero2.PrintStatsInfo();
+
+        Paladin knight = new Paladin("sir megacool", huntingBow);
+        knight.PrintStatsInfo();
+
+        //directionLight = GameObject.Find("Directional Light");
+
+        lightTransform = directionLight.GetComponent<Transform>();
+        Debug.Log(lightTransform.localPosition);
+    }
+
+    void GetComponentExample()
+    {
+        camTransform = this.GetComponent<Transform>();
+        Debug.Log(camTransform.localPosition);
+ 
+    }
+
+}
